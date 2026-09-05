1184918d3d6c52705a783c035110a1f57923b450
Annie Jenisha <merlinepeter12@gmail.com>
2026-09-05 07:35:50 +0000

    Fix the OCR Issue

--- holec_trading/__init__.py
+++ holec_trading/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.0.1"
+import aiohttp
+import asyncio
+
+# Compatibility patch for modern aiohttp versions removing SocketTimeoutError
+if not hasattr(aiohttp, "SocketTimeoutError"):
+    aiohttp.SocketTimeoutError = asyncio.TimeoutError
